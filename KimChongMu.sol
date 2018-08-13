04e2a67306d529d98da635919676835fe2c4b4c0
Rinuys <dlghdtjr1492@naver.com>
2018-08-13 21:22:32 +0900

    meeting 기능 추가

--- KimChongMu.sol
+++ KimChongMu.sol
@@ -1,51 +1,53 @@
-pragma solidity ^0.4.24;
+pragma solidity "0.4.24";
 
 contract KimChongMu{                // KimChongMu contract
-    struct Club{                    // 동아리 구조체
-        string id;                  // 동아리의 id
-        address[] member;
-        mapping(address => uint8) authority;
-        uint32 numberOfMember;      // 동아리 멤버의 수
-        bytes32[] meeting;
-        uint8 numberOfMeeting;
-        uint256 balance;            // 동아리의 회비
+    struct Club{                    // 클럽 구조체
+        string id;                  // 클럽의 id(string)
+        address[] member;           // 클럽에 가입된 멤버
+        mapping(address => uint8) authority; // 각 멤버의 authority를 저장
+        uint32 numberOfMember;      // 클럽 멤버의 수
+        bytes32[] meeting;          // 클럽에 등록된 모임
+        uint8 numberOfMeeting;      // 모임의 수
+        uint256 balance;            // 클럽의 총 지분
     }
     struct Member{                  // 멤버의 구조체
-        address id;                 // 멤버의 id(바이트32)
-        string[] club;
-        uint32 numberOfClub;        // club
-        bytes32[] meeting;
-        uint8 numberOfMeeting;
+        address id;                 // 멤버의 id(주소)
+        string[] club;              // 멤버가 포함된 클럽
+        uint32 numberOfClub;        // 클럽의 수
+        bytes32[] meeting;          // 멤버가 진행중인 모임
+        uint8 numberOfMeeting;      // 모임의 수
     }
-    struct Meeting{
-        string id;
-        string clubId;
-        uint256 time;
-        uint256 balance;
-        mapping(address => MemberState) memberState;
-        address[] member;
-        uint32 numberOfMember;
+    struct Meeting{                 // 모임의 구조체(식별자:keccak256(id+clubId+time))
+        string id;                  // 모임의 id(string)
+        string clubId;              // 모임이 소속된 클럽의 id
+        uint256 time;               // 모임시간
+        uint256 value;              // 예치해야하는 금액
+        uint256 totalBalance;       // 모임의 지분
+        uint8 state;                // 모임의 상태
+                                    // 0:모임생성, 1:예치금받기, 2:모임유효, 3:모임끝
+                                    // state=0까지 멤버추가가능(1부터 추가불가)
+        mapping(address => MemberState) memberState; // 각 멤버들의 상태
+        address[] member;           // 모임에 포함된 멤버
+        uint32 numberOfMember;      // 멤버의 수
     }
-    struct MemberState{
-        uint256 stake;
-        uint8 state;
+    struct MemberState{             // 멤버의 상태 구조체(모임)
+        uint256 stake;              // 멤버의 지분
+        uint8 state;                // 멤버의 상태
     }
     
-    mapping(bytes32 => Club) public club;   // club들을 저장하는 매핑변수
-    string[] internal clubIdArr;           // club들의 id를 저장하는 배열
-    uint256 internal numberOfClub = 0;     // 동아리의 수
+    mapping(bytes32 => Club) public club;       // 클럽(전역)
+    string[] internal clubIdArr;                // 클럽들의 id를 저장
+    uint256 internal numberOfClub = 0;          // 클럽의 수
     
-    mapping(address => Member) public member;
-    address[] internal memberIdArr;
-    uint256 internal numberOfMember = 0;
+    mapping(address => Member) public member;   // 멤버(전역)
+    address[] internal memberIdArr;             // 멤버들의 id를 저장
+    uint256 internal numberOfMember = 0;        // 멤버의 수
     
-    mapping(bytes32 => Meeting) public meeting;
-    bytes32[] internal meetingIdArr;
-    uint256 internal numberOfMeeting;
+    mapping(bytes32 => Meeting) public meeting; // 모임(전역)
+    bytes32[] internal meetingIdArr;            // 모임의 id를 저장
+    uint256 internal numberOfMeeting;           // 모임의 수
     
-    uint8 internal numberOfRule = 2;        // 사용 가능한 회칙의 개수 
-    
-    event transferTo(string _from, string _to, uint256 _value);     // 송금할 경우 이벤트 호출 
+    event transferTo(address _from, address _to, uint256 _value); // 송금할 경우 이벤트 호출 
     
     function stringToBytes32(string _id) 
         internal 
@@ -54,37 +56,46 @@ contract KimChongMu{                // KimChongMu contract
     {                                       // string형식의 id를
         return keccak256(bytes(_id));       // byte32형식으로 변환
     }                                       // (keccak256)
+
+    function argToBytes32(string _clubId, string _meetingId, uint256 _time)
+        internal
+        pure
+        returns(bytes32)
+    {
+        return keccak256(abi.encodePacked(_clubId, _meetingId, _time));
+    }
     
     function getContractBalance() 
         public 
         view 
         returns(uint)
-    {                                       // 전체 balance를 출력
+    {                                       // 전체 CA의 balance를 반환
         return address(this).balance;
     }
     
-    function isMemberIdExist(address _memberId)              // 멤버의 아이디가 있는지 체크하는 함수
+    function isMemberIdExist(address _memberId)         // 멤버의 아이디가 있는지 체크하는 함수
         internal 
         view 
         returns(bool)
     {
         uint i;
-        for(i = 0 ; i < numberOfMember ; i++){                 // .memberIdArr에 memberId가 있는지 체크
+        for(i = 0 ; i < numberOfMember ; i++){          // memberIdArr에 memberId가 있는지 체크
             if(memberIdArr[i] == _memberId){
                 break;
             }
         }
-        return (i != numberOfMember && numberOfMember != 0);                          // 있으면 true, 없으면 false
+        return (i != numberOfMember && numberOfMember != 0); // 있으면 true, 없으면 false
     }
     
     function isClubIdExist(string _clubId) 
         public 
         view 
         returns(bool)
-    {                               // 동아리 id가 존재하는지 검사하는 함수
+    {                                       // 클럽 id가 존재하는지 검사하는 함수
         uint i;
         for(i = 0 ; i < numberOfClub ; i++ ){
-            if(keccak256(clubIdArr[i]) == keccak256(_clubId)){ // clubIdArr에 id가 존재하는지 검사
+            if(keccak256(abi.encodePacked(clubIdArr[i])) == 
+                keccak256(abi.encodePacked(_clubId))){ // clubIdArr에 id가 존재하는지 검사
                 break;
             }
         }
@@ -92,18 +103,87 @@ contract KimChongMu{                // KimChongMu contract
     }
     
     function isMeetingIdExist(string _clubId, string _meetingId, uint256 _time)
-        public
+        public                              // 모임 id가 존재하는지 검사하는 함수
         view
         returns(bool)
     {
-        bytes32 tempId = keccak256(_clubId, _meetingId, _time);
+        bytes32 tempId = keccak256(
+            abi.encodePacked(_clubId, _meetingId, _time)
+        );
         uint i;
         for(i = 0 ; i < numberOfMeeting ; i++){
             if(meetingIdArr[i] == tempId){
                 break;
             }
         }
-        return (i != numberOfMeeting && numberOfMeeting != 0);
+        return (i != numberOfMeeting && numberOfMeeting != 0);  // 존재하면 true, 없으면 false
+    }
+
+    function isMeetingIdExist_Bytes32(bytes32 _meetingId)
+        public                              // 모임 id가 존재하는지 검사하는 함수
+        view
+        returns(bool)
+    {
+        uint i;
+        for(i = 0 ; i < numberOfMeeting ; i++){
+            if(meetingIdArr[i] == _meetingId){
+                break;
+            }
+        }
+        return (i != numberOfMeeting && numberOfMeeting != 0);  // 존재하면 true, 없으면 false
+    }
+
+    function isMemberInClub(string _clubId, address _memberId)
+        public
+        view
+        returns(bool)
+    {
+        require(isClubIdExist(_clubId));
+        require(isMemberIdExist(_memberId));
+        bytes32 clubId = stringToBytes32(_clubId);
+        uint number = club[clubId].numberOfMember;
+        uint i;
+        for(i = 0 ; i < number ; i++){
+            if(club[clubId].member[i] == _memberId){
+                break;
+            }
+        }
+        return (i != number && number != 0); // 있으면 true, 없으면 false
+    }
+
+    function isMemberInMeeting(string _clubId, string _meetingId, uint256 _time, address _memberId)
+        public
+        view
+        returns(bool)
+    {
+        require(isMemberIdExist(_memberId));
+        require(isMeetingIdExist(_clubId, _meetingId, _time));
+        bytes32 meetingId = argToBytes32(_clubId, _meetingId, _time);
+        uint number = meeting[meetingId].numberOfMember;
+        uint i;
+        for(i = 0 ; i < number ; i++){
+            if(meeting[meetingId].member[i] == _memberId){
+                break;
+            }
+        }
+        return (i != number && number != 0); // 있으면 true, 없으면 false
+    }
+
+    function isMemberInMeeting_Bytes32(address _memberId, bytes32 _meetingId)
+        public
+        view
+        returns(bool)
+    {
+        require(isMemberIdExist(_memberId));
+        require(isMeetingIdExist_Bytes32(_meetingId));
+        uint number = meeting[_meetingId].numberOfMember;
+        uint i;
+        for(i = 0 ; i < number ; i++){
+            if(meeting[_meetingId].member[i] == _memberId){
+                break;
+            }
+        }
+        return (i != number && number != 0); // 있으면 true, 없으면 false
     }
     
     function getNumberOfClub() 
--- MeetingManager.sol
+++ MeetingManager.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity "0.4.24";
 
 import "./ClubManager.sol";
 
@@ -6,7 +6,12 @@ contract MeetingManager is ClubManager{
     event meetingCreated(string _clubId, string _meetingId);
     event memberAddedInMeeting(string _clubId, string _meetingId, address _memberId);
     
-    function meetingCreate(string _clubId, string _meetingId, uint256 _time, uint256 _balance) 
+    function meetingCreate(                     // 모임 생성 함수
+        string _clubId,               
+        string _meetingId,                      // _meetingId : 모임이름
+        uint256 _time,                          // _time : 모임시간
+        uint256 _value                          // _value : 예치해야하는 금액
+    )                                           
         public 
     {
         require(isClubIdExist(_clubId));
@@ -14,16 +19,20 @@ contract MeetingManager is ClubManager{
         require(!isMeetingIdExist(_clubId, _meetingId, _time));
         //require(club[stringToBytes32(_clubId)].authority[msg.sender] == 1);
         Meeting memory temp;
-        bytes32 tempId = keccak256(_clubId, _meetingId, _time);
         temp.id = _meetingId;
         temp.time = _time;
-        temp.balance = _balance;
+        temp.value = _value;
         temp.clubId = _clubId;
+        temp.state = 0;
+        temp.totalBalance = 0;
+
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, _time);
         meeting[tempId] = temp;
         meeting[tempId].member.push(msg.sender);
         meeting[tempId].numberOfMember++;
         meetingIdArr.push(tempId);
         numberOfMeeting++;
+
         club[stringToBytes32(_clubId)].meeting.push(tempId);
         club[stringToBytes32(_clubId)].numberOfMeeting++;
         member[msg.sender].meeting.push(tempId);
@@ -36,15 +45,22 @@ contract MeetingManager is ClubManager{
         emit meetingCreated(_clubId, _meetingId);
     }
     
-    function addMemberInMeeting(string _clubId, string _meetingId, uint256 _time, address _memberId) 
+    function addMemberInMeeting(
+        string _clubId,
+        string _meetingId,
+        uint256 _time,
+        address _memberId
+    ) 
         public
     {
         require(isClubIdExist(_clubId));
         require(isMemberIdExist(msg.sender));
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, _time);
         require(isMeetingIdExist(_clubId, _meetingId, _time));
+        require(meeting[tempId].state == 0);
         //require(club[stringToBytes32(_clubId)].authority[msg.sender] == 1);
         
-        bytes32 tempId = keccak256(_clubId, _meetingId, _time);
+        
         meeting[tempId].member.push(_memberId);
         meeting[tempId].numberOfMember++;
         
@@ -58,6 +74,19 @@ contract MeetingManager is ClubManager{
         
         emit memberAddedInMeeting(_clubId, _meetingId, _memberId);
     }
+
+    function startTransfer(string _clubId, string _meetingId, uint256 _time)
+        public
+    {
+        require(isClubIdExist(_clubId));
+        require(isMeetingIdExist(_clubId, _meetingId, _time));
+        require(isMemberInMeeting(_clubId, _meetingId, _time, msg.sender));
+        //require(club[stringToBytes32(_clubId)].authority[msg.sender] == 1);
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, _time);
+        require(meeting[tempId].state == 0);
+
+        meeting[tempId].state = 1;
+    }
     
     function transferToMeeting(string _clubId, string _meetingId, uint256 _time) 
         public 
@@ -65,26 +94,34 @@ contract MeetingManager is ClubManager{
     {
         require(isClubIdExist(_clubId));
         require(isMeetingIdExist(_clubId, _meetingId, _time));
-        require(isMemberInMeeting(_clubId, _meetingId, _time, msg.sender));
-        bytes32 tempId = keccak256(_clubId, _meetingId, _time);
-        require(msg.value == meeting[tempId].balance);
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, _time);
+        require(isMemberInMeeting_Bytes32(msg.sender, tempId));
+        require(meeting[tempId].state == 1);
+        require(msg.value >= meeting[tempId].value);
         
-        msg.sender.transfer(address(this).balance);
-        
-        meeting[tempId].balance += msg.value;
+        meeting[tempId].totalBalance += msg.value;
         meeting[tempId].memberState[msg.sender].stake = msg.value;
         
         club[stringToBytes32(_clubId)].balance += msg.value;
         
         emit transferTo(msg.sender, address(this), msg.value);
+
+        if(checkMeetingBalance(tempId) == true){
+            meeting[tempId].state = 2;
+        }
     }
     
-    function isMemberInMeeting(string _clubId, string _meetingId, uint256 _time, address _memberId) 
+    function isMemberInMeeting(
+        string _clubId,
+        string _meetingId,
+        uint256 _time, 
+        address _memberId
+    ) 
         public 
         view
         returns(bool)
     {
-        bytes32 tempId = keccak256(_clubId, _meetingId, _time);
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, _time);
         uint tempNumber = meeting[tempId].numberOfMember;
         uint i;
         for(i = 0 ; i < tempNumber ; i++){
@@ -94,4 +131,51 @@ contract MeetingManager is ClubManager{
         }
         return (i != tempNumber && tempNumber != 0);
     }
+
+    function checkMeetingBalance(bytes32 _meetingId) 
+        internal 
+        view
+        returns(bool)
+    {
+        uint check=0;
+        address[] memory member = meeting[_meetingId].member;
+        for(uint i = 0 ; i < meeting[_meetingId].numberOfMember ; i++){
+            if(meeting[_meetingId].memberState[member[i]].stake >= meeting[_meetingId].value){
+                check++;
+            }
+        }
+        if(check == meeting[_meetingId].numberOfMember){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    function getMeetingInfo(string _clubId, string _meetingId, uint256 time)
+        public
+        view
+        returns(string, uint256, uint256, uint8, uint32)
+    {
+        bytes32 tempId = argToBytes32(_clubId, _meetingId, time);
+        Meeting memory temp = meeting[tempId];
+        return(temp.id, temp.value, temp.totalBalance, temp.state, temp.numberOfMember);
+    }
+    
+/*
+    function revertBalance(string _clubId, bytes32 _meetingId) 
+        internal
+    {                                                                       // 멤버들의 지분을 반환
+        require(isMemberIdExist(_memberId, _clubId));
+        require(club[stringToId(_clubId)].member[stringToId(_memberId)].authority == 1);
+        clubBalance -= club[stringToId(_clubId)].balance;
+        for(uint8 i = 0; i < club[stringToId(_clubId)].numberOfMember; i++) {
+            club[stringToId(_clubId)].member[club[stringToId(_clubId)].memberIdArr[i]].account.transfer(
+                club[stringToId(_clubId)].member[stringToId(_memberId)].balance);
+        }
+    }
+
+    function deleteMeeting(string _clubId, string _meetingId, uint256 time){
+
+    }*/
 }
